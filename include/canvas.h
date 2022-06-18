a16a168e4a6d5c89284e4b2eba8bc2664e59bbf2
Evan Ng <75152390+evan-ng@users.noreply.github.com>
2022-06-18 17:15:29 -0400

    Create Canvas class

---
+++ include/canvas.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+class Canvas {
+private:
+    const int width, height, num_channels;
+    std::vector<unsigned char> & image_data;
+
+public:
+    Canvas(int width, int height, int num_channels);
+    ~Canvas();
+
+    void plot_pixel(Point3 position, Colour colour);
+    bool write_ppm(const std::string & filename);
+
+    int get_width();
+    int get_height();
+    int get_num_channels();
+};
--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 
 int main(int argc, char *argv[])
 {
+    const int width = 1920;
+    const int height = 1080;
+
     return 0;
 }
 
